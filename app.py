e6d9342a56ff481807e549c393958eeebb390e1a
B.Karthikeya <karthikbatta143@gmail.com>
2026-07-01 19:58:53 +0530

    checking the api

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ groq_api_key=os.getenv("GROQ_API_KEY")
 llm_model=os.getenv("LLM_MODEL")
 embedding_model=os.getenv("EMBEDDING_MODEL")
 
+llm = ChatGroq(
+    groq_api_key=groq_api_key,
+    model_name="llama-3.3-70b-versatile"
+)
+print(llm.invoke("how is Baahubali?"))
