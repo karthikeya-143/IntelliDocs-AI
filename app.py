33466198f24d73ec333506fe61dfcc01e8e956bc
B.Karthikeya <karthikbatta143@gmail.com>
2026-07-01 19:42:41 +0530

    Initial IntelliDocs AI setup

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+llm_model=os.getenv("LLM_MODEL")
+embedding_model=os.getenv("EMBEDDING_MODEL")
+
